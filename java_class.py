2a0904a00ba248413cb1e319360d26459ed227a9
Legati Matteo <58668065+legatimatteo@users.noreply.github.com>
2024-04-18 23:31:40 +0200

    added subdir check

--- java_class.py
+++ java_class.py
@@ -6,15 +6,15 @@ class java_class:
 
     def __str__(self):
         class_name_str = "\n".join([f"visibility={name['visibility']} name={name['name']}" for name in self.name])
-        attributes_str = "\n".join([f"visibility={attr['visibility']} data_type={attr['data_type']} name={attr['name']}" for attr in self.attributes])
-        methods_str = "\n".join([f"visibility={method['visibility']} return_type={method['return_type']} name={method['name']}(p={method['parameters']})" for method in self.methods])
+        attributes_str = "\n".join([f"visibility={attr['visibility']} static={attr['static']} final={attr['final']} data_type={attr['data_type']} name={attr['name']}" for attr in self.attributes])
+        methods_str = "\n".join([f"visibility={method['visibility']} static={method['static']} return_type={method['return_type']} name={method['name']}(p={method['parameters']})" for method in self.methods])
 
         return f"Class: {class_name_str}\nAttributes:\n{attributes_str}\nMethods:\n{methods_str}"
     
     def toPlantUML(self):
         class_name = "class " + ''.join([f"{name['name']}" for name in self.name]) + '{'
-        attributes = "".join([f"\n\t {attr['visibility']} {attr['name']} : {attr['data_type']}" for attr in self.attributes])
-        methods = "".join([f"\n\t {method['visibility']} {method['name']}({method['parameters']})" for method in self.methods])
+        attributes = "".join([f"\n\t {attr['visibility']} {'{static}' if 'static' in attr and attr['static'] else ''} {attr['name']} : {attr['data_type']} {'= ' + attr['var_value'] if 'var_value' in attr and attr['var_value'] else ''} {'{readOnly}' if 'final' in attr and attr['final'] else ''}" for attr in self.attributes])
+        methods = "".join([f"\n\t {method['visibility']} {'{static}' if 'static' in method and method['static'] else ''} {method['name']}({method['parameters']})" for method in self.methods])
         close_bracket = "\n}\n"
 
         return f"{class_name} {attributes} {methods} {close_bracket}"
--- plantuml_generator.py
+++ plantuml_generator.py
@@ -19,7 +19,8 @@ def write_file(filename, classes):
     with open(filename, 'w') as file:
         file.write('@startuml\n')
         for j_class in classes:
-            file.write(j_class.toPlantUML())
+            if j_class is not None:
+                file.write(j_class.toPlantUML())
 
         file.write('@enduml\n')
 
@@ -27,10 +28,14 @@ def parse_java_file(file_path):
     with open(file_path, 'r') as file:
         java_code = file.read()
 
-    visibility_regex = '|'.join(VISIBILITY_MODIFIER.keys())
+    print(file_path)
+
+    map_regex = '(?:\s*<\s*\w+\s*(?:,\s*\w+\s*)*>)'
 
     # find class name and visibility
-    class_name_visibility = re.search(r'(' + visibility_regex + ')?\s*class\s+(\w+)', java_code)
+    class_name_visibility = re.search(r'(public|private|protected)?\s*class\s+(\w+)', java_code)
+    print("CLASS NAME\n")
+    print(class_name_visibility)
     if class_name_visibility:
         name = []
         class_visibility = class_name_visibility.group(1)
@@ -41,34 +46,43 @@ def parse_java_file(file_path):
         return
 
     # finds attributes
-    attributes = re.findall(r'(' + visibility_regex + ')?\s+(\w+)\s+(\w+);', java_code)
+    #attributes = re.findall(r'(public|private|protected)\s*(static)?\s*(final)?\s+(\w+' + map_regex + r'?)\s+(\w+);', java_code)
+    attributes = re.findall(r'(public|private|protected)\s*(static)?\s*(final)?\s+(\w+' + map_regex + r'?)\s+(\w+)(?:\s*=\s*)?(.*?)?;', java_code)
+
+    print("ATTRIBUTES\n")
+    print(attributes)
     if attributes:
         attributes_list = []
         for attr in attributes:
-            if len(attr) == 3:
-                visibility, data_type, attr_name = attr
+            if len(attr) >= 3:
+                visibility, static, final, data_type, attr_name, value = attr
                 if visibility != '':
                     attributes_list.append(
-                        {"visibility" : VISIBILITY_MODIFIER[visibility], "data_type" : data_type, "name" : attr_name}
+                        {"visibility" : VISIBILITY_MODIFIER[visibility], "static" : static, "final" : final, "data_type" : data_type, "name" : attr_name, "var_value" : value}
                     )
 
     methods_list = []
     #find constructors
     methods = re.findall(r'(public)\s+(\w+)\s*\((.*?)\)', java_code)
-    visibility, method_name, parameters = methods[0]
-    methods_list.append(
-        {"visibility" : VISIBILITY_MODIFIER[visibility], "return_type" : "constructor", "name" : method_name, "parameters" : parameters}
-    )
+    print("CONSTRUCTORS\n")
+    print(methods)
+    if methods:
+        visibility, method_name, parameters = methods[0]
+        methods_list.append(
+            {"visibility" : VISIBILITY_MODIFIER[visibility], "return_type" : "constructor", "name" : method_name, "parameters" : parameters}
+        )
     # find methods
-    methods = re.findall(r'(' + visibility_regex + ')\s+(\w+)?\s+(\w+)\s*\((.*?)\)', java_code)
+    methods = re.findall(r'(public|private|protected)\s*(static)?\s+(\w+)\s+(\w+)\s*\((.*?)\)', java_code)
+    print("METHODS\n")
+    print(methods)
     if methods:
         for method in methods:
             # if len(method) == 4:
             #print(method)
-            visibility, return_type, method_name, parameters = method
+            visibility, static, return_type, method_name, parameters = method
             if return_type != 'new':
                 methods_list.append(
-                    {"visibility" : VISIBILITY_MODIFIER[visibility], "return_type" : return_type, "name" : method_name, "parameters" : parameters}
+                    {"visibility" : VISIBILITY_MODIFIER[visibility], "static" : static, "return_type" : return_type, "name" : method_name, "parameters" : parameters}
                 )
 
     return java_class(name, attributes_list, methods_list)
@@ -98,12 +112,24 @@ if __name__ == '__main__':
             sys.exit(1)
 
         for file in os.listdir(project_path):
-            classes_list.append(parse_java_file(project_path + '/' + os.fsdecode(file)))
+            if os.path.isfile(project_path + '/' + os.fsdecode(file)):
+                if file.endswith('.java') and not file.startswith('.'):
+                    classes_list.append(parse_java_file(project_path + '/' + os.fsdecode(file)))
     
     elif option == '-r' or option == '--recursive':
         if not os.path.exists(project_path):
             print(project_path + ", folder does not exist.")    
             sys.exit(1)
+        
+        file_list = []
+        for root, dirs, files in os.walk(project_path):
+            for file in files:
+                if file.endswith('.java') and not file.startswith('.'):
+                    file_list.append(os.path.join(root, file))
+
+        file_list.remove
+        for file in file_list:
+            classes_list.append(parse_java_file(file))
 
     else:
         print_help()
@@ -115,5 +141,4 @@ if __name__ == '__main__':
     filename = 'plantuml_code.txt'
 
     write_file(OUTPUT_FOLDER + filename, classes_list)
-    print(option)
     print("File created.")
